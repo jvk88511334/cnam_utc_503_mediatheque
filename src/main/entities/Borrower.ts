1b8f6bc4af13341d860e334351111333792806b0
jerome <jvk@abes.fr>
2019-11-30 11:12:41 +0100

    Creation de l'entite Borrower

--- src/main/entities/Borrower.ts
+++ src/main/entities/Borrower.ts
@@ -1,3 +1,34 @@
-export class Borrower{
+import {Borrowing} from "./Borrowing";
 
+export class Borrower{
+    // Attributes
+    private id: number;
+    private name: string;
+    private _firstname: string;
+    private email: string;
+
+    private _borrowingList: Borrowing[];
+
+    // Constructor
+    constructor(id: number, name: string, firstname: string, email: string) {
+        this.id = id;
+        this.name = name;
+        this._firstname = firstname;
+        this.email = email;
+        this._borrowingList = []; //A la creation de l'emprunteur, sa liste est vide
+    }
+
+    // Getters and setters
+    get borrowingList(): Borrowing[] {
+        return this._borrowingList;
+    }
+
+    // Methods
+
+    /**
+     * Methode permettant d'ajouter un emprunt à l'emprunteur
+     */
+    addBorrowing(paramBorrowing: Borrowing){
+        this._borrowingList.push(paramBorrowing);
+    }
 }
